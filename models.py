911338230cadd2052c5c86b46d724b9d729117b8
Hans Hazairi <hanshazairi@gmail.com>
2022-01-17 16:21:47 +0800

    chore: add /create_user endpoint

--- models.py
+++ models.py
@@ -1,4 +1,5 @@
 from sqlalchemy import BigInteger, Column, String
+from sqlalchemy.orm import validates
 
 from database import Base
 
@@ -7,3 +8,53 @@ class User(Base):
     __tablename__ = 'user'
     id = Column(BigInteger, primary_key=True)
     password = Column(String(60), nullable=False)
+    name = Column(String(64), nullable=False)
+    discriminator = Column(String(4), nullable=False)
+    display_name = Column(String(64), nullable=False)
+
+    @validates('id')
+    def validate_id(self, key, id):
+        if type(id) is not int:
+            raise TypeError
+
+        if len(str(id)) != 18:
+            raise ValueError
+
+        return id
+
+    @validates('password')
+    def validate_password(self, key, password):
+        if type(password) is not str:
+            raise TypeError
+
+        return password
+
+    @validates('name')
+    def validate_name(self, key, name):
+        if type(name) is not str:
+            raise TypeError
+
+        if len(name) > 64:
+            raise ValueError
+
+        return name
+
+    @validates('discriminator')
+    def validate_name(self, key, discriminator):
+        if type(discriminator) is not str:
+            raise TypeError
+
+        if len(discriminator) != 4:
+            raise ValueError
+
+        return discriminator
+
+    @validates('display_name')
+    def validate_display_name(self, key, display_name):
+        if type(display_name) is not str:
+            raise TypeError
+
+        if len(display_name) > 64:
+            raise ValueError
+
+        return display_name
